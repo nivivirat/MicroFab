d5b5afc55cb145f21b6c8b995cec6b9904ace859
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2024-01-19 18:52:19 +0530

    Update appli2.jsx

--- src/appli2.jsx
+++ src/appli2.jsx
@@ -41,26 +41,74 @@ export default function Appli1() {
      
     setSelectedImage(imageId);
   };
+  const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+
+    return (
+      <div className="text-black p-4 w-48 ml-8 rounded-[16px]">
+        <div className="flex flex-col items-start ">
+          <button className="text-xl font-bold mb-2" onClick={toggleMenu}>
+           <Icon icon="uil:align-justify" />
+          </button>
+  
+          {/* Navigation Menu */}
+          <div className={`mt-2 bg-[#E8E8E9] w-48 mb-4 ${menuOpen ? 'block' : 'hidden'}`}>
+            <button
+              onClick={() => showImage("image1")}
+              className="block text-black hover:bg-gray-300 ml-4 "
+            >
+              Creams
+            </button>
+            <button
+          onClick={() => showImage("image2")}
+          
+           className="block text-black hover:bg-gray-300 ml-4 "
+        >
+          Serums
+        </button>
+        <button
+          onClick={() => showImage("image3")}
+           className="block text-black hover:bg-gray-300 ml-4 "
+        >
+        Gels
+        </button>
+        <button
+          onClick={() => showImage("image4")}
+           className="block text-black hover:bg-gray-300 ml-4 "
+        >
+            Tonics
+        </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <>
       {/* <div> */}
       <div>
-        <span className="qaz">
-        <div className="qa">
-          <h3 className="mb-6 w-full pb-8 lg:text-7xl md: text-5xl sm:text-5xl leading-tight text-[#8AA6AA]  df animate__animated animate__fadeInLeft animate__delay-0s"><strong>
-          Packaging Solutions for Cosmetics</strong>
+      <span className="qaz">
+        <div className="qa -mt-64">
+        <h3 className="mb-6 w-full pb-8 lg:text-6xl md: text-4xl sm:text-4xl leading-tight text-[#8AA6AA]  animate__animated animate__fadeInLeft animate__delay-0s df">
+       <strong>
+          Packaging Solutions for the Food Industry</strong>
           </h3>
           <span>
-            <h6
-              className="lg:text-3xl md:text-2xl sm:text-xl leading-tight text-[#8AA6AA]  sd animate__animated animate__fadeInRight animate__delay-0s"
+          <h6
+              className="lg:text-2xl md:text-2xl mt-2 sm:text-xl leading-tight text-[#8AA6AA]  sd animate__animated animate__fadeInRight animate__delay-0s"
               color="blue"
             >
-           Our technology can be considered for a user-friendly packaging solution for serums, tonics or gels. The cosmetics will stay fresh and a need to add preservatives will not arise. </h6>{" "}
+         MicroFab transforms the realm of food packaging with its solutions designed to preserve freshness, ensure safety, and enhance convenience. Our technologies cater to the unique needs, offering precision and innovation.
+            </h6>{" "}
             {/* <br></br> */}
           </span>
           </div>
          
-          <button className="bg-[#8AA6AA] ee  text-white font-bold py-3 px-20 rounded m-2">
+          <button className="bg-[#8AA6AA] md:w-48 ee mt-24 sm:ml-2 lg:ml-12 md:ml-10  text-white font-bold py-3 px-20 rounded m-2 ">
   Explore
 </button>
 
@@ -96,39 +144,7 @@ export default function Appli1() {
      <h3 className="mb-2 pb-2 text-5xl ml-8 leading-tight text-black animate__animated animate__fadeInLeft animate__delay-2s"><strong>
             Types of Packaging</strong>
           </h3>
-      <div className="navbar1">
-        <spacer></spacer><spacer></spacer>{ }
-        <button
-          onClick={() => showImage("image1")}
-          style={{ marginRight: "4%" }}
-        >
-          Creams
-        </button>
-        <button
-          onClick={() => showImage("image2")}
-          
-          style={{ marginRight: "4%" }}
-        >
-          Serums
-        </button>
-        <button
-          onClick={() => showImage("image3")}
-          style={{ marginRight: "4%" }}
-        >
-          Gels
-        </button>
-        <button
-          onClick={() => showImage("image4")}
-          style={{ marginRight: "4%" }}
-        >
-          Tonics
-        </button>
-        {/* {selectedImage && (
-        <img src={`path/to/${selectedImage}.jpg`} alt={`Image ${selectedImage}`} />
-      )}
-      {/* Display the text */}
-     
-      </div>
+          <NavBar />
       <br></br>
 
       <div
@@ -447,9 +463,9 @@ export default function Appli1() {
           Regardless of which instance you’re at; a wealth of experience and a plethora of services ensures you to move ahead. Maybe so what we offer:
           </h5>
           </center><br></br>
-          <a className="text-xs  leading-tight mb-4   text-white  tt" href="https://google.com">Product solutions<Icon icon="ph:arrow-right-light" /></a><br></br>
-          <a className="text-xs  leading-tight mb-4 text-white  tt" href="https://google.com">Turnkey solutions <Icon icon="ph:arrow-right-light" /></a><br></br>
-          <a className="text-xs  leading-tight mb-4  text-white  tt" href="https://google.com">Service solutions <Icon icon="ph:arrow-right-light" /></a><br></br>
+          <a className="text-xs  leading-tight mb-4   text-white  tt" href="/product">Product solutions</a><br></br>
+          <a className="text-xs  leading-tight mb-4 text-white  tt" href="/TurnKeySolutions">Turnkey solutions </a><br></br>
+          <a className="text-xs  leading-tight mb-4  text-white  tt" href="/service">Service solutions </a><br></br>
           <br></br>
           
           </div>
@@ -467,5 +483,3 @@ export default function Appli1() {
     </>
   );
 }
-
-
