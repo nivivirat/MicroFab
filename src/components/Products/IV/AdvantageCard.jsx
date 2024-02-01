c21f297c3a93a20c1595d8174357a80f93e55753
nivivirat <imniveditha1188@gmail.com>
2024-02-01 19:52:49 +0530

    IV advantages align

--- src/components/Products/IV/AdvantageCard.jsx
+++ src/components/Products/IV/AdvantageCard.jsx
@@ -1,6 +1,6 @@
 export default function AdvantageCard({ heading, content }) {
     return (
-      <div className="xl:h-[200px] h-[100%] justify-center w-auto rounded-lg p-4 flex flex-col bg-white">
+      <div className="xl:h-[200px] h-[100%] w-auto rounded-lg p-4 flex flex-col bg-white">
         <div className="mb-2 font-semibold">{heading}</div>
         <div className="pr-7">{content}</div>
       </div>
