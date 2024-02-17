aa93de0c929ebbe6ae0ad91143612d8bd4dcd07a
nivivirat <imniveditha1188@gmail.com>
2024-02-17 22:36:33 +0530

    Refactor social media links in Footer component

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -1,19 +1,32 @@
-import Points from "./Points";
-import dot from "../../../assets/Footer/dot.svg";
-import insta from "../../../assets/Footer/instagram.svg";
-import tikTok from "../../../assets/Footer/tik-tok.svg";
-import facebook from "../../../assets/Footer/facebook.svg";
-import GetinTouchWithUs from "../../contact/GetinTouchWithUs/GetinTouchWithUs";
-import ContactForm from "../../contact/Contact_form/Contact_form";
-import logo from "../../../assets/footerlogo.svg";
 import { Icon } from "@iconify/react";
+import { onValue, ref } from "firebase/database";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { db } from "../../../../firebase";
+import logo from "../../../assets/footerlogo.svg";
+import ContactForm from "../../contact/Contact_form/Contact_form";
+import GetinTouchWithUs from "../../contact/GetinTouchWithUs/GetinTouchWithUs";
 
 export default function Footer() {
 
   const location = useLocation();
 
   console.log(location.pathname === "/contact");
+
+  const [socialMediaLinks, setSocialMediaLinks] = useState({});
+
+  useEffect(() => {
+    const socialMediaRef = ref(db, 'SocialMediaLinks');
+    onValue(socialMediaRef, (snapshot) => {
+      if (snapshot.exists()) {
+        setSocialMediaLinks(snapshot.val());
+      }
+    });
+  }, []);
+
+  const handleLinkClick = (link) => {
+    window.open(link, '_blank');
+  };
 
   return (
     <div className="w-full font-['ClashDisplay']">
@@ -87,77 +100,16 @@ export default function Footer() {
             <div className=" md:hidden md:h-[w-[30%]] md:w-[10%] w-[h-[90%]] h-[2px] bg-white md:mt-0 md:mb-0 mt-[22px] mb-[15px]"></div>
             {/* vertical line */}
             <div className="w-[1px] flex md:flex-row flex-row h-[w-[30%]] bg-white"></div>
-            {/* <div className="w-[100%] md:w-[40%] md:text-[18px] md:flex flex-col justify-between">
-              <div className="md:flex md:flex-row justify-between flex flex-row">
-                <div className="">
-                  <div className="flex flex-row">
-                    <img src={dot} alt="dot" className=""></img>
-                    <p className="md:ml-4">Lorem ipsum</p>
-                  </div>
-                  <div className="md:mt-5 mt-4">
-                    <p>Lorem ipsum</p>
-                    <p>Lorem ipsum</p>
-                    <p>Lorem ipsum</p>
-                    <p>Lorem ipsum</p>
-                    <p>Lorem ipsum</p>
-                  </div>
-                </div>
-                <div className="md:flex md:flex-row flex flex-row md:place-items-start place-items-center md:gap-2 gap-2">
-                  <img
-                    src={tikTok}
-                    alt="tiktok logo"
-                    className="border-2 border-white h-10 rounded-full p-2"
-                  ></img>
-                  <img
-                    src={insta}
-                    alt="insta logo"
-                    className="border-2 border-white h-10 rounded-full p-2"
-                  ></img>
-                  <img
-                    src={facebook}
-                    alt="facebook logo"
-                    className="border-2 border-white h-10 rounded-full p-2"
-                  ></img>
-                </div>
-              </div>
-              <div className="md:flex md:flex-col">
-                <hr className="h-px my-8 bg-white border-0"></hr>
-
-                <div className="md:text-[14px] md:flex md:flex-row justify-between">
-                  <p>Lorem ipsum:</p>
-                  <div className="md:flex md:flex-row flex flex-row">
-                    <p className="font-bold">Lorem ipsum:</p>
-                    <p> lorem ipsum</p>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-            {/* <img src={facebook} alt="facebook logo"></img> */}
-            <div className="md:flex md:flex-row flex flex-row md:place-items-start place-items-center md:gap-2 gap-2">
-              <a href="" className="md:ml-10">
-                <Icon
-                  icon="mdi:linkedin"
-                  className="border-2 border-white text-white rounded-full h-10 p-2 w-10"
-                />
-              </a>
-              <a href="" className="">
-                <Icon
-                  icon="mdi:instagram"
-                  className="border-2 border-white text-white rounded-full h-10 p-2 w-10"
-                />
-              </a>
-              <a href="" className="">
-                <Icon
-                  icon="mingcute:youtube-line"
-                  className="border-2 border-white text-white rounded-full h-10 p-2 w-10"
-                />
-              </a>
-              <a href="" className="">
-                <Icon
-                  icon="ic:baseline-facebook"
-                  className="border-2 border-white text-white rounded-full h-10 p-2 w-10"
-                />
-              </a>
+            <div className="md:flex md:flex-row flex flex-row md:place-items-start place-items-center gap-2">
+              {Object.entries(socialMediaLinks).map(([platform, link]) => (
+                <a key={platform} href={link} className="" target="_blank" rel="noopener noreferrer">
+                  <Icon
+                    icon={`mdi:${platform}`}
+                    className="border-2 border-white text-white rounded-full h-10 p-2 w-10 cursor-pointer"
+                    onClick={() => handleLinkClick(link)}
+                  />
+                </a>
+              ))}
             </div>
           </div>
 
