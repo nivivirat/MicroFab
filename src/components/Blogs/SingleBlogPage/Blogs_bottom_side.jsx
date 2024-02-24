81e47aa059614625056a4a37c64f44c6c839a2c7
nivivirat <imniveditha1188@gmail.com>
2024-02-24 14:42:20 +0530

    Refactor blog card layout and styling

--- src/components/Blogs/SingleBlogPage/Blogs_bottom_side.jsx
+++ src/components/Blogs/SingleBlogPage/Blogs_bottom_side.jsx
@@ -54,7 +54,32 @@ export default function blogs_bottom_side() {
           <div className="m-3 flex flex-col gap-5 justify-center md:place-items-start place-items-center">
             {mediaData.slice(0, 4).map((blog, index) => (
               <div key={index} className="blog-card">
-                {/* Rest of your component code */}
+                <div className="w-full rounded-lg shadow-md p-4">
+                  <img
+                    src={blog.img}
+                    alt="Article Thumbnail"
+                    className="w-[full] h-[200px] object-center object-cover rounded-lg"
+                  />
+                  <div className="p-2 flex flex-col gap-4">
+                    <div className="flex flex-row place-items-center">
+                      <div className="w-[3px] mr-2 h-[18px] bg-[#8AA6AA]"></div>
+                      <p className="text-left text-gray-500 text-[14px]">
+                        {blog.description}
+                      </p>
+                    </div>
+                    <p className="text-left text-black text-[16px] font-black">
+                      {blog.heading}
+                    </p>
+                    <a href={blog.description} className="flex flex-row place-items-center gap-3">
+                      <p className="text-[#8AA6AA] font-extrabold">Read more</p>
+                      <img
+                        src={arrow_right}
+                        className="bg-[#8AA6AA] h-6 p-0.5 rounded-full"
+                        alt="Read more"
+                      />
+                    </a>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
@@ -72,7 +97,32 @@ export default function blogs_bottom_side() {
           <div className="m-3 flex flex-col gap-5 justify-center md:place-items-start place-items-center">
             {mediaData.slice(0, 4).map((blog, index) => (
               <div key={index} className="blog-card">
-                {/* Rest of your component code */}
+                <div className="w-full flex flex-row rounded-lg shadow-md p-4">
+                  <img
+                    src={blog.img}
+                    alt="Article Thumbnail"
+                    className="w-[49%] object-cover rounded-lg"
+                  />
+                  <div className="p-2 flex flex-col gap-4">
+                    <p className="text-left text-black text-[12px] font-black">
+                      {blog.heading}
+                    </p>
+                    <div className="flex flex-row place-items-center">
+                      <div className="w-[3px] mr-2 h-[18px] bg-[#8AA6AA]"></div>
+                      <p className="text-left text-gray-500 text-[12px]">
+                        {blog.description}
+                      </p>
+                    </div>
+                    <a href={blog.dir} className="flex flex-row place-items-center gap-3">
+                      <p className="text-[#8AA6AA] font-extrabold">Read more</p>
+                      <img
+                        src={arrow_right}
+                        className="bg-[#8AA6AA] h-6 p-0.5 rounded-full"
+                        alt="Read more"
+                      />
+                    </a>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
