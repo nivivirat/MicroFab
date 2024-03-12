fbcdac665e33053277aca89bd973f626ae135159
nivivirat <imniveditha1188@gmail.com>
2024-03-12 10:29:06 +0530

    Update Firebase configuration

--- src/components/Home/right/BFS_FFS.jsx
+++ src/components/Home/right/BFS_FFS.jsx
@@ -5,14 +5,20 @@ import "../../../App.css";
 
 export default function BFS({ content }) {
   const imageToShow = content === "BFS" ? img1 : content === "FFS" ? img2 : null;
+  const c = content === "BFS" ? "Project" : "Advisory"
+  const d = content === "BFS" ? "Management" : "Services"
+  // const c = content === "BFS" ? "BFS" : "FFS"
 
   return (
     <a href={"/#/" + content} className="transition-all duration-300 ease-in-out hover:shadow-md hover:bg-[#a0b4b7] font-['ClashDisplay'] w-[100%] h-[45%] md:w-[100%] md:h-[46%] md:mr-2 mr-10 bg-[#cdd7d8] rounded-[16px] mt-[8px] flex flex-col overflow-hidden">
-      <div className="h-[100%] flex flex-row items-center place-items-center justify-center gap-5">
+      <div className="h-[100%] flex flex-row items-center place-items-center justify-between">
         <div className="h-[35px] w-[35px] bg-[#8aa6aa] rounded-full flex justify-center">
           <img src={imageToShow} alt="" className="w-[20px]" />
         </div>
-        <p className="text-[20px]">{content}</p>
+        <div>
+          <p className="text-[16px] text-center">{c}</p>
+          <p className="text-[16px] text-center">{d}</p>
+        </div>
         <div className="h-[35px] w-[35px] bg-[#8aa6aa] rounded-full flex justify-center place-items-center">
           <img src={arrow} alt="" className="h-[20px] w-[20px]" />
         </div>
