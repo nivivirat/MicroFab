f0a6897d882fec205d50888da32615740cbe4e9e
nivivirat <imniveditha1188@gmail.com>
2024-03-18 12:47:06 +0530

    Refactor BFS_FFS component to dynamically generate href value

--- src/components/Home/right/BFS_FFS.jsx
+++ src/components/Home/right/BFS_FFS.jsx
@@ -7,10 +7,16 @@ export default function BFS({ content }) {
   const imageToShow = content === "BFS" ? img1 : content === "FFS" ? img2 : null;
   const c = content === "BFS" ? "Project" : "Advisory"
   const d = content === "BFS" ? "Management" : "Services"
-  // const c = content === "BFS" ? "BFS" : "FFS"
 
+  let hrefValue = "/#/service";
+  if (content === "BFS") {
+    hrefValue += "#project";
+  } else if (content === "FFS") {
+    hrefValue += "#advisory";
+  }
+  
   return (
-    <a href={"/#/" + content} className="transition-all duration-300 ease-in-out hover:shadow-md hover:bg-[#a0b4b7] font-['ClashDisplay'] w-[100%] h-[45%] md:w-[100%] md:h-[46%] md:mr-2 mr-10 bg-[#cdd7d8] rounded-[16px] mt-[8px] flex flex-col overflow-hidden">
+    <a href={hrefValue} className="transition-all duration-300 ease-in-out hover:shadow-md hover:bg-[#a0b4b7] font-['ClashDisplay'] w-[100%] h-[45%] md:w-[100%] md:h-[46%] md:mr-2 mr-10 bg-[#cdd7d8] rounded-[16px] mt-[8px] flex flex-col overflow-hidden">
       <div className="h-[100%] flex flex-row items-center place-items-center justify-between pl-3">
         {/* <div className="h-[35px] w-[35px] bg-[#8aa6aa] rounded-full flex justify-center">
           <img src={imageToShow} alt="" className="w-[20px]" />
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './services.css';
 import pro from "./images/Frame 21516 (2).svg";
 import pro2 from "./images/Frame 21518.svg";
@@ -8,9 +8,9 @@ import pro6 from "./images/image 86.svg";
 import MedicalDevicesCard from "../MedicalDevices/MedicalDevicesCard";
 import Footer from "../Common/Footer/Footer";
 import ServicesJson from "./Services.json";
-import ServicesJson1 from "./services1.json"
-export default function Service() {
+import ServicesJson1 from "./services1.json";
 
+export default function Service() {
   const [openCardIndex, setOpenCardIndex] = useState(null);
 
   const handleCardToggle = (index) => {
@@ -21,31 +21,40 @@ export default function Service() {
     }
   };
 
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const id = hash.slice(1);
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth", block: "start" }); // Adjust scroll behavior
+      }
+    }
+  }, []);
+
   return (
     <>
-      {/* <NavbarDefault /> */}
-      <br></br><br></br><br></br><br></br>
-      <div className="serv1">
+      <br /><br /><br /><br />
+      <div id="service" className="serv1">
         <div>
-          <br></br><br></br>
+          <br /><br />
           <h3 className="mb-4 pb-4 text-3xl leading-tight text-[#8AA6AA] serv3">
             Service Solutions
           </h3>
-          <br></br>
+          <br />
 
-          <h3 className="mb-4  pb-4 text-3xl leading-tight text-black zx  animate__animated animate__fadeInLeft animate__delay-0s">
+          <h3 className="mb-4 pb-4 text-3xl leading-tight text-black zx animate__animated animate__fadeInLeft animate__delay-0s">
             Experience the difference with us as firmly stand with our motto to offer incredible services every step of the way
           </h3>
 
-          <h6 className="text-2xs leading-tight text-black serv2  animate__animated animate__fadeInRight animate__delay-0s" color="clack">
+          <h6 className="text-2xs leading-tight text-black serv2 animate__animated animate__fadeInRight animate__delay-0s" color="clack">
             While we dabble in providing exceptional customer service, you can connect with us for project management and contract manufacturing.
           </h6>
-          <br></br><br></br><br></br><br></br>
+          <br /><br /><br /><br /><br />
           <div className="serv4"></div>
-          <br></br>
+          <br />
 
-
-          <div className="flex flex-col place-items-center rounded-lg w-[90vw] justify-between px-10 text-black md:flex-row  animate__animated animate__fadeInLeft animate__delay-1s">
+          <div className="flex flex-col place-items-center rounded-lg w-[90vw] justify-between px-10 text-black md:flex-row animate__animated animate__fadeInLeft animate__delay-1s">
             <img
               className="h-64 w-full rounded-t-lg object-cover md:h-auto md:w-[300px] md:rounded-none md:rounded-l-lg order-2 md:order-"
               src={pro}
@@ -53,19 +62,20 @@ export default function Service() {
             />
             <div className="flex flex-col justify-start order-1 md:order-1 md:pr-[200px]">
               <h3 className="font-bold mb-2 pb-4 tracking-wide text-5xl leading-tight text-black">
-                Project Construction & Management </h3>
+                Project Construction & Management
+              </h3>
               <h6 className="text-2xs text-[#67675F] leading-7 text-left" color="clack">
                 As you get in touch with us to outline more about your project, our dynamic and forward-thinking team takes charge of overseeing and guiding the project execution from its earliest stages. Ultimately, our focus remains steadfast on ensuring customer delight and fulfilling business requirements by delivering a feasible project within the allocated budget, while prioritizing its functionality.
               </h6>
-
             </div>
           </div>
-          <br></br><br></br>
-        </div></div>
-      <div className="md:pl-6 flex flex-wrap gap-7 md:gap-5 md:gap-y-4 md:p-0 p-8 md:place-items-start place-items-center md:justify-start justify-center  animate__animated animate__fadeInRight animate__delay-2s">
+          <br /><br />
+        </div>
+      </div>
+      <div id="project" className="md:pl-6 flex flex-wrap gap-7 md:gap-5 md:gap-y-4 md:p-0 p-8 md:place-items-start place-items-center md:justify-start justify-center animate__animated animate__fadeInRight animate__delay-2s">
         {ServicesJson.map((device, index) => (
           <MedicalDevicesCard
-            key={index} // Ensure each card has a unique key
+            key={index}
             index={index}
             heading={device.heading}
             content={device.content}
@@ -75,31 +85,30 @@ export default function Service() {
           />
         ))}
       </div>
-      <br></br><br></br>
+      <br /><br />
       <div className="serv1">
         <div>
           <div className="serv4"></div>
-          <br></br>
-
+          <br />
 
           <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row animate__animated animate__fadeInLeft animate__delay-3s">
             <img
-              className="h-64 w-96 rounded-t-lg object-cover md:h-auto md:w-120 md:rounded-none md:rounded-l-lg order-2 mmn animate__animated animate__fadeInLeft animate__delay-3s  md:order-"
+              className="h-64 w-96 rounded-t-lg object-cover md:h-auto md:w-120 md:rounded-none md:rounded-l-lg order-2 mmn animate__animated animate__fadeInLeft animate__delay-3s md:order-"
               src={pro2}
               alt=""
             />
             <div className="flex flex-col justify-start order-1 md:order-1">
               <h3 className="mb-2 pb-4 text-4xl font-bold leading-tight text-black">
-                Contract manufacturing</h3>
+                Contract manufacturing
+              </h3>
               <h6 className="text-2xs xz hj text-[#67675F] leading-7" color="clack">
-                When the entire gamut of production operational activities becomes cumbersome, you can approach us at that instance. With MicroFab, you can work closely with professionals that possess specialized skills and the expertise. With advanced technologies, you can further expect higher quality products and an improvement in the production process.
+                When the entire gamut of production operational activities becomes cumbersome, you can approach us at that instance. With MicroFab, you can work closely with professionals that possess specialized skills and expertise. With advanced technologies, you can further expect higher quality products and an improvement in the production process.
               </h6>
             </div>
           </div>
-          <br></br>
+          <br />
 
-
-          <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row  animate__animated animate__fadeInRight animate__delay-4s">
+          <div className="flex flex-col rounded-lg text-black md:max-w-5xl md:flex-row animate__animated animate__fadeInRight animate__delay-4s">
             <img
               className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-96 md:rounded-none md:rounded-l-lg order-2 bbv md:order-"
               src={pro4}
@@ -111,10 +120,9 @@ export default function Service() {
               alt=""
             />
           </div>
-          <br></br><br></br><br></br><br></br>
+          <br /><br /><br /><br />
           <div className="md:pt-3"></div>
-          <br></br>
-
+          <br />
 
           <div className="flex flex-col justify-center rounded-lg w-screen text-black md:max-w-5xl md:flex-row">
             <img
@@ -122,22 +130,24 @@ export default function Service() {
               src={pro6}
               alt=""
             />
-            <div className="flex flex-col w-[50vw] justify-start order-1 md:order-2 op mr-6  animate__animated animate__fadeInLeft animate__delay-4s">
+            <div className="flex flex-col w-[50vw] justify-start order-1 md:order-2 op mr-6 animate__animated animate__fadeInLeft animate__delay-4s">
               <div className="serv4 mb-4"></div>
 
               <h3 className="mb-2 pb-4 text-4xl leading-tight font-bold tracking-wide text-black">
-                Advisory Services </h3>
+                Advisory Services
+              </h3>
               <h6 className="text-2xs text-[#67675F] leading-7 md:w-[60vw]" color="clack">
-                When perplexity is still reigning while the project implementation is yet to commence, we offer advisory services with experience. While we help you with technology transfer and ensure that everything you need is incorporated on premise, we conduct market research and a diligent study about sites across different locations. For project success, we also help you sift through the technical complexities so that you can focus on what really matters.
+                When perplexity is still reigning while the project implementation is yet to commence, we offer advisory services with experience. While we help you with technology transfer and ensure that everything you need is incorporated on-premise, we conduct market research and a diligent study about sites across different locations. For project success, we also help you sift through the technical complexities so that you can focus on what really matters.
               </h6>
             </div>
           </div>
-        </div></div>
-      <br></br><br></br><br></br>
-      <div className="md:pl-6 flex flex-wrap gap-7 md:gap-5 md:gap-y-4 md:p-0 p-8 md:place-items-start place-items-center md:justify-start justify-center  animate__animated animate__fadeIn animate__delay-5s">
+        </div>
+      </div>
+      <br /><br /><br /><br />
+      <div className="md:pl-6 flex flex-wrap gap-7 md:gap-5 md:gap-y-4 md:p-0 p-8 md:place-items-start place-items-center md:justify-start justify-center animate__animated animate__fadeIn animate__delay-5s">
         {ServicesJson1.map((device, index) => (
           <MedicalDevicesCard
-            key={index} // Ensure each card has a unique key
+            key={index}
             index={index}
             heading={device.heading}
             content={device.content}
@@ -147,10 +157,7 @@ export default function Service() {
           />
         ))}
       </div>
-
-
-
       <Footer />
     </>
-  )
-};
+  );
+}
